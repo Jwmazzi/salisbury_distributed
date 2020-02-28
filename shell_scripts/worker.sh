44e6968cb4e09639ee04be5f195b158c2bca214d
Jeffrey Scarmazzi <jeff8977@esri.com>
2020-02-28 07:47:24 -0500

    Worker Script & Empty Config

--- shell_scripts/worker.sh
+++ shell_scripts/worker.sh
@@ -4,10 +4,4 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -P /o
 bash /opt/Miniconda3-latest-Linux-x86_64.sh -b -p /opt/miniconda3
 
 /opt/miniconda3/bin/conda create --name worker python=3.6 -y
-/opt/miniconda3/envs/worker/bin/pip install newspaper3k boto3 celery pandas awscliyum install wget -y
-
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -P /opt
-bash /opt/Miniconda3-latest-Linux-x86_64.sh -b -p /opt/miniconda3
-
-/opt/miniconda3/bin/conda create --name worker python=3.6 -y
 /opt/miniconda3/envs/worker/bin/pip install newspaper3k boto3 celery pandas awscli
